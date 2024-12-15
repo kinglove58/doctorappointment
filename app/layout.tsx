cdfedd59578fe2f3370ae6f378459963a37e9551
kinglove58 <obafemielijahsunday@gmail.com>
2024-12-15 13:46:29 +0100

    Implement theme support and add button component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "next-themes";
 
 const fontSans = Plus_Jakarta_Sans({
   variable: "--font-sans",
@@ -21,7 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen bg-dark-300 ")}>{children}</body>
+      <body className={cn("min-h-screen bg-dark-300 ")}>
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
