290e99c93bec137866a27ece1f1c0d6cbc4d303f
kinglove58 <obafemielijahsunday@gmail.com>
2024-12-20 15:35:03 +0100

    Auto commit at 12/20/2024, 3:35:02 PM

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -7,9 +7,9 @@ import { getUser } from "@/lib/actions/patient.actions";
 const Register = async ({ params: { userId } }: SearchParamProps) => {
   const user = await getUser(userId);
   return (
-    <div className="flex h-screen max-h-screen ">
-      <section className="container my-auto remove-scrollbar">
-        <div className="sub-container max-h-[496px]">
+    <div className="flex h-screen max-h-screen">
+      <section className="container remove-scrollbar">
+        <div className="sub-container max-h-[860px] flex-1 flex-col py-10">
           <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
